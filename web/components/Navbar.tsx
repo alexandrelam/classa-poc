cad317b99c6b76803ada74e20f941c183c7b24b5
Alexandre Lam <alexandre.lam@doctolib.com>
2023-04-10 00:17:02 +0200

    settings for themes

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -1,62 +1,41 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Navbar() {
-  const themes = [
-    "light",
-    "dark",
-    "cupcake",
-    "bumblebee",
-    "emerald",
-    "corporate",
-    "synthwave",
-    "retro",
-    "cyberpunk",
-    "valentine",
-    "halloween",
-    "garden",
-    "forest",
-    "aqua",
-    "lofi",
-    "pastel",
-    "fantasy",
-    "wireframe",
-    "black",
-    "luxury",
-    "dracula",
-    "cmyk",
-    "autumn",
-    "business",
-    "acid",
-    "lemonade",
-    "night",
-    "coffee",
-    "winter",
-  ];
-
-  function handleThemeChange(e: React.ChangeEvent<HTMLSelectElement>) {
-    const newTheme = e.target.value;
-    document.querySelector("html")?.setAttribute("data-theme", newTheme);
-  }
-
   return (
     <div className="navbar bg-base-100 border-b border-neutral border-opacity-10 flex justify-between">
       <Link href="/" className="btn btn-ghost normal-case text-xl">
         Classa
       </Link>
-      <select
-        className="select select-primary w-full max-w-xs"
-        onChange={handleThemeChange}
-      >
-        {themes.map((theme, index) => (
-          <option
-            key={index}
-            defaultValue={theme}
-            selected={theme === "garden"}
-          >
-            {theme}
-          </option>
-        ))}
-      </select>
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <Image
+              src="/profile.webp"
+              alt="profile picture"
+              width={40}
+              height={40}
+            />
+          </div>
+        </label>
+        <ul
+          tabIndex={0}
+          className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>Profile</a>
+          </li>
+          <li>
+            <Link className="justify-between" href="/profile/settings">
+              Settings
+              <span className="badge">New</span>
+            </Link>
+          </li>
+          <li>
+            <a>Logout</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           },
         ]}
       >
-        <div>Loading...</div>;
+        <div>Loading...</div>
       </MainLayout>
     );
 
