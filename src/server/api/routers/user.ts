0ef8228f25b87875867ca29fd6e24c1c5cad97bc
Alexandre LAM <alexandrelam@outlook.com>
2023-04-14 20:24:56 +0200

    login mutation

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -5,6 +5,27 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const userRouter = createTRPCRouter({
+  login: publicProcedure
+    .input(z.object({ email: z.string(), password: z.string() }))
+    .mutation(async ({ input }) => {
+      if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET not set");
+
+      const user = await User.findOne({ email: input.email });
+      if (!user) throw new Error("User not found");
+
+      const passwordIsValid = await bcrypt.compare(
+        input.password,
+        user.password
+      );
+
+      if (!passwordIsValid) throw new Error("Invalid password");
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+
+      return { token, user: { name: user.name, email: user.email } };
+    }),
   register: publicProcedure
     .input(
       z.object({
